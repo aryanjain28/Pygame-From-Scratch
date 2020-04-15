39b7b9c9da60987be75f3863eadbe27872b26a91
Aryan Jain <aryanjn807@gmail.com>
2020-04-16 02:03:54 +0530

    First commit

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import pygame
 
 pygame.init()
 
-window = pygame.display.set_mode((1500,1000))
+window = pygame.display.set_mode((2000,2000))
 pygame.display.set_caption("Caption")
 
-x,y,w,h = 10, 10, 20, 20
-v = 1
+x,y,w,h = 0, 0, 20, 20
+v = 5
 run = True
+
+isJump = False
+jumpCount = 10
 
 while run:
     
-    pygame.time.delay(1)
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
@@ -19,15 +22,30 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_UP]:
-        y-=v
-    if keys[pygame.K_DOWN]:
-        y+=v
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x<500-w:
         x+=v
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and  x>v:
         x-=v
     
+    if not isJump:
+        if keys[pygame.K_UP] and y>v :
+            y-=v
+        if keys[pygame.K_DOWN] and y<500-h:
+            y+=v
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y-=(jumpCount**2) * 0.5 * neg
+            jumpCount-=1
+        else:
+            isJump = False
+            jumpCount = 10
+
+
     window.fill((0,0,0))
     pygame.draw.rect(window, (255, 0 ,0 ), (x,y,w,h))
     pygame.display.update()
